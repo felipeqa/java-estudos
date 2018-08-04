2ceb8713f13ead7fdaabe020ac8c1a6c3e742965
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2018-08-04 20:36:13 -0300

    add nested for

--- src/TestaFor.java
+++ src/TestaFor.java
@@ -4,6 +4,7 @@ public class TestaFor {
 
         int total = 23;
 
+        //  |inicializa a variavel| condição | incrementa
         for(int contador = 0; contador <= 10 ; contador++ ){
             System.out.println(contador);
             System.out.println(total);
