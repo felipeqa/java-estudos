68693782a9f7d19c5cdecbe1177590836981a4c1
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2018-08-04 20:24:48 -0300

    add for

---
+++ src/TestaFor.java
@@ -0,0 +1,12 @@
+public class TestaFor {
+
+    public static void main(String[] args) {
+
+        int total = 23;
+
+        for(int contador = 0; contador <= 10 ; contador++ ){
+            System.out.println(contador);
+            System.out.println(total);
+        }
+    }
+}
